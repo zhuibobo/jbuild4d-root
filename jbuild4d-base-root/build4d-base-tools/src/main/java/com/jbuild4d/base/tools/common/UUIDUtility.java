cf77f76261674514aa5d6b2bbb9acc4ba6f871dc
zhuibobo <zhuibobo@qq.com>
2018-06-27 22:26:45 +0800

    bobo v1

---
+++ jbuild4d-base-root/build4d-base-tools/src/main/java/com/jbuild4d/base/tools/common/UUIDUtility.java
@@ -0,0 +1,25 @@
+package com.jbuild4d.base.tools.common;
+
+import java.util.UUID;
+
+public class UUIDUtility {
+
+    /**
+     * 获取UUID
+     *
+     * @return
+     */
+    public static String getUUID(){
+        return  UUID.randomUUID().toString();
+    }
+
+    public static String getUUIDNotSplit(){
+        String uuid=getUUID();
+        return uuid.replaceAll("-","");
+    }
+
+    public static String getFullTimeStr(){
+        return DateUtility.getDate_yyyyMMddHHmmssSSS();
+    }
+}
+
