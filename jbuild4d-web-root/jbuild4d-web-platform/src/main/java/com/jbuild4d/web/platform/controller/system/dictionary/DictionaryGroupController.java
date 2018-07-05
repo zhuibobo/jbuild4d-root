4011ee83cd1b1b2b2bb27ca256041de23475db60
zhuibobo <zhuibobo@qq.com>
2018-07-05 18:31:31 +0800

    zhuangrb

---
+++ jbuild4d-web-root/jbuild4d-web-platform/src/main/java/com/jbuild4d/web/platform/controller/system/dictionary/DictionaryGroupController.java
@@ -0,0 +1,10 @@
+package com.jbuild4d.web.platform.controller.system.dictionary;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhuangrb
+ * Date: 2018/7/5
+ * To change this template use File | Settings | File Templates.
+ */
+public class DictionaryGroupController {
+}
