03901fbe9bc16559b8cc2acd10b10bb78a8654dd
zhuibobo <zhuibobo@qq.com>
2018-07-15 16:04:39 +0800

    zhuangrb

--- jbuild4d-platform-root/jbuild4d-platform-system/src/main/java/com/jbuild4d/platform/system/service/impl/DevDemoGenListServiceImpl.java
+++ jbuild4d-platform-root/jbuild4d-platform-system/src/main/java/com/jbuild4d/platform/system/service/impl/DevDemoGenListServiceImpl.java
@@ -1,13 +1,10 @@
 package com.jbuild4d.platform.system.service.impl;
 
-import com.jbuild4d.base.dbaccess.dao.BaseMapper;
 import com.jbuild4d.base.dbaccess.dao.DevDemoGenListMapper;
 import com.jbuild4d.base.dbaccess.dbentities.DevDemoGenListEntity;
-import com.jbuild4d.base.dbaccess.dbentities.DictionaryGroupEntity;
 import com.jbuild4d.base.service.ISQLBuilderService;
 import com.jbuild4d.base.service.impl.BaseServiceImpl;
 import com.jbuild4d.platform.system.service.IDevDemoGenListService;
-import com.jbuild4d.platform.system.service.IDictionaryGroupService;
 import org.mybatis.spring.SqlSessionTemplate;
 
 /**
