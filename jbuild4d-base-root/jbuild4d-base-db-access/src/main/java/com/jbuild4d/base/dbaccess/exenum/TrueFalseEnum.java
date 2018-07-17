d960cc99babb94f5983396f8ecaaeb36634670b3
zhuibobo <zhuibobo@qq.com>
2018-07-17 16:38:01 +0800

    zhuangrb

--- jbuild4d-base-root/jbuild4d-base-db-access/src/main/java/com/jbuild4d/base/dbaccess/exenum/TrueFalseEnum.java
+++ jbuild4d-base-root/jbuild4d-base-db-access/src/main/java/com/jbuild4d/base/dbaccess/exenum/TrueFalseEnum.java
@@ -1,5 +1,8 @@
 package com.jbuild4d.base.dbaccess.exenum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  * User: zhuangrb
@@ -7,19 +10,41 @@ package com.jbuild4d.base.dbaccess.exenum;
  * To change this template use File | Settings | File Templates.
  */
 public enum TrueFalseEnum implements BaseEnum<EnableTypeEnum, Integer>{
-    ;
+    True(1,true,"是"),
+    False(0,false,"否");
+
+    private Integer value;
+    private boolean boolValue;
+    private String displayName;
 
     @Override
     public Integer getValue() {
-        return null;
+        return value;
     }
 
     @Override
     public String getDisplayName() {
-        return null;
+        return displayName;
     }
 
-    /*private Integer value;
-    private String displayName;*/
+    public boolean getBoolValue(){
+        return boolValue;
+    }
 
+    private TrueFalseEnum(int value,boolean boolValue,String displayName) {
+        this.value=value;
+        this.boolValue=boolValue;
+        this.displayName=displayName;
+    }
+
+    static Map<Integer,TrueFalseEnum> enumMap=new HashMap<Integer, TrueFalseEnum>();
+    static{
+        for(TrueFalseEnum type: TrueFalseEnum.values()){
+            enumMap.put(type.getValue(), type);
+        }
+    }
+
+    public static TrueFalseEnum getEnum(Integer value) {
+        return enumMap.get(value);
+    }
 }
