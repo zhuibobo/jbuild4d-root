2c84f4e0405077a8c979319e1121734d9dc72427
zhuibobo <zhuibobo@qq.com>
2018-07-27 21:32:28 +0800

    zhuangrb

---
+++ jbuild4d-base-root/jbuild4d-base-db-access/src/main/java/com/jbuild4d/base/dbaccess/dao/OrganTypeMapper.java
@@ -0,0 +1,13 @@
+package com.jbuild4d.base.dbaccess.dao;
+
+import com.jbuild4d.base.dbaccess.dbentities.OrganEntity;
+import com.jbuild4d.base.dbaccess.dbentities.OrganTypeEntity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhuangrb
+ * Date: 2018/7/27
+ * To change this template use File | Settings | File Templates.
+ */
+public interface OrganTypeMapper extends BaseMapper<OrganTypeEntity> {
+}
