eb5bea98765cca5c93b5210a1df1e2e92a43bcd6
zhuibobo <zhuibobo@qq.com>
2018-07-30 18:07:10 +0800

    zhuangrb

---
+++ jbuild4d-base-root/jbuild4d-base-db-access/src/main/java/com/jbuild4d/base/dbaccess/dao/DatabaseServiceLinkMapper.java
@@ -0,0 +1,12 @@
+package com.jbuild4d.base.dbaccess.dao;
+
+import com.jbuild4d.base.dbaccess.dbentities.DatabaseServiceLinkEntity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhuangrb
+ * Date: 2018/7/30
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DatabaseServiceLinkMapper extends BaseMapper<DatabaseServiceLinkEntity> {
+}
