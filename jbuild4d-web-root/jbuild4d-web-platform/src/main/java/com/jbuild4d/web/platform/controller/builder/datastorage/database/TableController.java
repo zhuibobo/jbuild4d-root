b254225a34fd2e455b91e05cd8ad7144fe5c2b0e
zhuibobo <zhuibobo@qq.com>
2018-07-31 18:33:01 +0800

    zhuangrb

--- jbuild4d-web-root/jbuild4d-web-platform/src/main/java/com/jbuild4d/web/platform/controller/builder/datastorage/database/TableController.java
+++ jbuild4d-web-root/jbuild4d-web-platform/src/main/java/com/jbuild4d/web/platform/controller/builder/datastorage/database/TableController.java
@@ -1,6 +1,7 @@
 package com.jbuild4d.web.platform.controller.builder.datastorage.database;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.jbuild4d.base.dbaccess.dbentities.TableFieldEntity;
 import com.jbuild4d.base.service.exception.JBuild4DGenerallyException;
 import com.jbuild4d.base.service.general.JB4DSession;
 import com.jbuild4d.base.service.general.JB4DSessionUtility;
@@ -9,7 +10,9 @@ import com.jbuild4d.base.tools.common.JsonUtility;
 import com.jbuild4d.base.tools.common.StringUtility;
 import com.jbuild4d.base.tools.common.UUIDUtility;
 import com.jbuild4d.platform.builder.exenum.TableFieldTypeEnum;
+import com.jbuild4d.platform.builder.service.ITableFieldService;
 import com.jbuild4d.web.platform.model.JBuild4DResponseVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,6 +32,9 @@ import java.util.Map;
 @RequestMapping(value = "/PlatForm/Builder/DataStorage/DataBase/Table")
 public class TableController {
 
+    @Autowired
+    ITableFieldService tableFieldService;
+
     @RequestMapping(value = "EditTable", method = RequestMethod.GET)
     public ModelAndView editTable(String recordId, String op) throws IllegalAccessException, InstantiationException, JsonProcessingException {
         ModelAndView modelAndView=new ModelAndView("Builder/DataStorage/DataBase/TableEdit");
@@ -39,5 +45,19 @@ public class TableController {
     @ResponseBody
     public JBuild4DResponseVo getFieldDataType() throws JBuild4DGenerallyException {
         return JBuild4DResponseVo.success("", TableFieldTypeEnum.getJsonString());
+    }
+
+    @RequestMapping(value = "/GetFieldTemplateName")
+    @ResponseBody
+    public JBuild4DResponseVo getFieldTemplateName(){
+        List<String> namesList=tableFieldService.getFieldTemplateName();
+        return JBuild4DResponseVo.success("",namesList);
+    }
+
+    @RequestMapping(value = "/GetTemplateFieldsByName")
+    @ResponseBody
+    public JBuild4DResponseVo getFieldTemplateName(String templateName){
+        List<TableFieldEntity> tableFieldEntityList=tableFieldService.getFieldTemplateName(templateName);
+        return JBuild4DResponseVo.success("",tableFieldEntityList);
     }
 }
