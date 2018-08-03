5e06eb22236fc56bc38c45bcca7e116f83a20b32
zhuibobo <zhuibobo@qq.com>
2018-08-03 18:00:11 +0800

    zhuangrb

---
+++ jbuild4d-platform-root/jbuild4d-platform-system/src/main/java/com/jbuild4d/platform/system/service/impl/EnvVariableServiceImpl.java
@@ -0,0 +1,12 @@
+package com.jbuild4d.platform.system.service.impl;
+
+import com.jbuild4d.platform.system.service.IEnvVariableService;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhuangrb
+ * Date: 2018/8/3
+ * To change this template use File | Settings | File Templates.
+ */
+public class EnvVariableServiceImpl implements IEnvVariableService {
+}
---
+++ jbuild4d-web-root/jbuild4d-web-platform/src/main/java/com/jbuild4d/web/platform/controller/selectview/SelectViewController.java
@@ -0,0 +1,15 @@
+package com.jbuild4d.web.platform.controller.selectview;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhuangrb
+ * Date: 2018/8/3
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequestMapping(value = "/PlatForm/SelectView/")
+public class SelectViewController {
+}
