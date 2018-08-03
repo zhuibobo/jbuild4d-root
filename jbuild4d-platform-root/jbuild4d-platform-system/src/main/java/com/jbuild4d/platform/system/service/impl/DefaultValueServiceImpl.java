e4044ccbb65d599c14fa4b6fa93161d4e0b3357e
zhuibobo <zhuibobo@qq.com>
2018-08-03 17:01:03 +0800

    zhuangrb

---
+++ jbuild4d-platform-root/jbuild4d-platform-system/src/main/java/com/jbuild4d/platform/system/service/impl/DefaultValueServiceImpl.java
@@ -0,0 +1,10 @@
+package com.jbuild4d.platform.system.service.impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhuangrb
+ * Date: 2018/8/3
+ * To change this template use File | Settings | File Templates.
+ */
+public class DefaultValueServiceImpl {
+}
