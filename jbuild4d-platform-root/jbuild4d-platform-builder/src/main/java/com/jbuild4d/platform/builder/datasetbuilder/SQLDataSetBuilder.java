1ebf2b7b4bb9fd9e1e28754684f93820bab75680
zhuibobo <zhuibobo@qq.com>
2018-08-08 17:47:46 +0800

    zhuangrb

--- jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/datasetbuilder/SQLDataSetBuilder.java
+++ jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/datasetbuilder/SQLDataSetBuilder.java
@@ -3,17 +3,28 @@ package com.jbuild4d.platform.builder.datasetbuilder;
 import com.jbuild4d.base.service.general.JB4DSession;
 import com.jbuild4d.platform.builder.vo.DataSetColumnVo;
 import com.jbuild4d.platform.builder.vo.DataSetVo;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.BinaryExpression;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.parser.CCJSqlParserManager;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.update.Update;
+import net.sf.jsqlparser.util.TablesNamesFinder;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.rowset.SqlRowSetMetaData;
 
+import java.io.StringReader;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -41,13 +52,84 @@ public class SQLDataSetBuilder {
             @Override
             public Object doInPreparedStatement(PreparedStatement ps) throws SQLException, DataAccessException {
                 ResultSetMetaData resultSetMetaData=ps.getMetaData();
+                //resultSetMetaData.
                 for(int i=1;i<=resultSetMetaData.getColumnCount();i++){
-                    //System.out.println(resultSetMetaData.getColumnName(i));
+                    System.out.println(resultSetMetaData.getColumnName(i));
+                    //System.out.println(resultSetMetaData.getTableName(i));
                 }
                 return null;
             }
         });
         dataSetVo.setColumnVoList(dataSetColumnVoList);
+
+        CCJSqlParserManager pm = new CCJSqlParserManager();
+        Statement statement = null;
+        try {
+            statement = pm.parse(new StringReader(sql));
+        } catch (JSQLParserException e) {
+            e.printStackTrace();
+        }
+
+        if (statement instanceof Select) {
+            //获得Update对象
+            Select selectStatement = (Select) statement;
+            TablesNamesFinder tablesNamesFinder = new TablesNamesFinder();
+            List tableList = tablesNamesFinder.getTableList(selectStatement);
+            for (Iterator iter = tableList.iterator(); iter.hasNext();) {
+                String tableName=iter.next().toString();
+                System.out.println(tableName);
+            }
+        }
+
         return dataSetVo;
+    }
+
+    /**
+     * 获得where条件字段中列名，以及对应的操作符
+     * @Title: getColumnName
+     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @param @param expression
+     * @param @param allColumnNames
+     * @param @return 设定文件
+     * @return StringBuffer 返回类型
+     * @throws
+     */
+    private static StringBuffer getColumnName(Expression expression,StringBuffer allColumnNames) {
+
+        String columnName = null;
+        if(expression instanceof BinaryExpression){
+            //获得左边表达式
+            Expression leftExpression = ((BinaryExpression) expression).getLeftExpression();
+            //如果左边表达式为Column对象，则直接获得列名
+            if(leftExpression  instanceof Column){
+                //获得列名
+                columnName = ((Column) leftExpression).getColumnName();
+                allColumnNames.append(columnName);
+                allColumnNames.append(":");
+                //拼接操作符
+                allColumnNames.append(((BinaryExpression) expression).getStringExpression());
+                //allColumnNames.append("-");
+            }
+            //否则，进行迭代
+            else if(leftExpression instanceof BinaryExpression){
+                getColumnName((BinaryExpression)leftExpression,allColumnNames);
+            }
+
+            //获得右边表达式，并分解
+            Expression rightExpression = ((BinaryExpression) expression).getRightExpression();
+            if(rightExpression instanceof BinaryExpression){
+                Expression leftExpression2 = ((BinaryExpression) rightExpression).getLeftExpression();
+                if(leftExpression2 instanceof Column){
+                    //获得列名
+                    columnName = ((Column) leftExpression2).getColumnName();
+                    allColumnNames.append("-");
+                    allColumnNames.append(columnName);
+                    allColumnNames.append(":");
+                    //获得操作符
+                    allColumnNames.append(((BinaryExpression) rightExpression).getStringExpression());
+                }
+            }
+        }
+        return allColumnNames;
     }
 }
--- jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/vo/DataSetVo.java
+++ jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/vo/DataSetVo.java
@@ -1,5 +1,7 @@
 package com.jbuild4d.platform.builder.vo;
 
+import com.jbuild4d.base.dbaccess.dbentities.DatasetEntity;
+
 import java.util.List;
 
 /**
@@ -8,7 +10,7 @@ import java.util.List;
  * Date: 2018/8/8
  * To change this template use File | Settings | File Templates.
  */
-public class DataSetVo {
+public class DataSetVo extends DatasetEntity {
 
     List<DataSetColumnVo> columnVoList;
 
