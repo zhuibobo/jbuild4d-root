7e1e76db49f5aa894b3f796f2a8dc4d25aa3f3ad
zhuibobo <zhuibobo@qq.com>
2018-08-08 17:23:07 +0800

    zhuangrb

--- jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/vo/DataSetColumnVo.java
+++ jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/vo/DataSetColumnVo.java
@@ -1,12 +1,12 @@
 package com.jbuild4d.platform.builder.vo;
 
+import com.jbuild4d.base.dbaccess.dbentities.DatasetColumnEntity;
+
 /**
  * Created with IntelliJ IDEA.
  * User: zhuangrb
  * Date: 2018/8/8
  * To change this template use File | Settings | File Templates.
  */
-public class DataSetColumnVo {
-    String columnName;
-    String caption;
+public class DataSetColumnVo extends DatasetColumnEntity {
 }
