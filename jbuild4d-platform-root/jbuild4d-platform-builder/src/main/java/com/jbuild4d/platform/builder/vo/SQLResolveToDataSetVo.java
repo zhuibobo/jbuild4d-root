a782f5ff3a9bb310ac93833fb369e8ceebc6e74d
zhuibobo <zhuibobo@qq.com>
2018-08-17 15:02:03 +0800

    zhuangrb

---
+++ jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/vo/SQLResolveToDataSetVo.java
@@ -0,0 +1,46 @@
+package com.jbuild4d.platform.builder.vo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhuangrb
+ * Date: 2018/8/17
+ * To change this template use File | Settings | File Templates.
+ */
+public class SQLResolveToDataSetVo {
+    String sqlWithEnvText;
+    String sqlWithEnvValue;
+    String sqlWithEnvRunningValue;
+    DataSetVo dataSetVo;
+
+    public String getSqlWithEnvText() {
+        return sqlWithEnvText;
+    }
+
+    public void setSqlWithEnvText(String sqlWithEnvText) {
+        this.sqlWithEnvText = sqlWithEnvText;
+    }
+
+    public String getSqlWithEnvValue() {
+        return sqlWithEnvValue;
+    }
+
+    public void setSqlWithEnvValue(String sqlWithEnvValue) {
+        this.sqlWithEnvValue = sqlWithEnvValue;
+    }
+
+    public String getSqlWithEnvRunningValue() {
+        return sqlWithEnvRunningValue;
+    }
+
+    public void setSqlWithEnvRunningValue(String sqlWithEnvRunningValue) {
+        this.sqlWithEnvRunningValue = sqlWithEnvRunningValue;
+    }
+
+    public DataSetVo getDataSetVo() {
+        return dataSetVo;
+    }
+
+    public void setDataSetVo(DataSetVo dataSetVo) {
+        this.dataSetVo = dataSetVo;
+    }
+}
