0a2d9d48d30afe0baf8d027625ce2b5d10351358
zhuibobo <zhuibobo@qq.com>
2018-08-30 11:51:26 +0800

    zhuangruibo

---
+++ jbuild4d-web-root/jbuild4d-web-platform/src/test/java/com/jbuild4d/controller/InitializationSystemControllerTest.java
@@ -0,0 +1,11 @@
+package com.jbuild4d.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhuangrb
+ * Date: 2018/8/30
+ * To change this template use File | Settings | File Templates.
+ */
+public class InitializationSystemControllerTest {
+
+}
