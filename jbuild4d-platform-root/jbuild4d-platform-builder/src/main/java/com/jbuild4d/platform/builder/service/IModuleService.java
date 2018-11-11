60f4edc33d5be6d0faba6f84d985f4799997ed4d
zhuibobo <zhuibobo@qq.com>
2018-11-11 21:57:55 +0800

    zhuangrb

---
+++ jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/service/IModuleService.java
@@ -0,0 +1,7 @@
+package com.jbuild4d.platform.builder.service;
+
+import com.jbuild4d.base.dbaccess.dbentities.builder.ModuleEntity;
+import com.jbuild4d.base.service.IBaseService;
+
+public interface IModuleService extends IBaseService<ModuleEntity> {
+}
