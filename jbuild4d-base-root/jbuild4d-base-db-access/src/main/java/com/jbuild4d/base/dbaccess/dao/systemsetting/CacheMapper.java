41be38cd4afac2d6c0dd9144849a44de5400426c
zhuibobo <zhuibobo@qq.com>
2018-11-21 10:19:53 +0800

    zhuangruibo

--- jbuild4d-base-root/jbuild4d-base-db-access/src/main/java/com/jbuild4d/base/dbaccess/dao/systemsetting/CacheMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jbuild4d.base.dbaccess.dao.systemsetting;
-
-import com.jbuild4d.base.dbaccess.dao.BaseMapper;
-import com.jbuild4d.base.dbaccess.dbentities.systemsetting.CacheEntity;
-
-public interface CacheMapper extends BaseMapper<CacheEntity> {
-}
