2eed1777ea053837480601f98352ee0822566628
zhuibobo <zhuibobo@qq.com>
2018-11-22 22:31:07 +0800

    zhuangrb

---
+++ jbuild4d-web-root/jbuild4d-web-platform/src/main/java/com/jbuild4d/web/platform/controller/builder/form/AppFormDesignController.java
@@ -0,0 +1,4 @@
+package com.jbuild4d.web.platform.controller.builder.form;
+
+public class AppFormDesignController {
+}
---
+++ jbuild4d-web-root/jbuild4d-web-platform/src/main/java/com/jbuild4d/web/platform/controller/builder/form/WebFormDesignController.java
@@ -0,0 +1,9 @@
+package com.jbuild4d.web.platform.controller.builder.form;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/PlatForm/Builder/WebFormDesign")
+public class WebFormDesignController {
+}
