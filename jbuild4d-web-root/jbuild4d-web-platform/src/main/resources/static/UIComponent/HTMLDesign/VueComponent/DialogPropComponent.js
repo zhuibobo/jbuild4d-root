f3c3ecc07f98a562ec9b16ce10743a6c3b509dc8
zhuibobo <zhuibobo@qq.com>
2018-11-27 22:42:56 +0800

    zhuangrb

--- jbuild4d-web-root/jbuild4d-web-platform/src/main/resources/static/UIComponent/HTMLDesign/VueComponent/DialogPropComponent.js
+++ jbuild4d-web-root/jbuild4d-web-platform/src/main/resources/static/UIComponent/HTMLDesign/VueComponent/DialogPropComponent.js
@@ -10,6 +10,10 @@ Vue.component("fd-control-bind-to", {
             bindToFieldDataLength: ""
         }
     },*/
+    mounted:function(){
+        //将当前对象附着到window上,提供给子窗体使用
+        window._SelectBindFieldObj = this;
+    },
     methods:{
         a:function () {
             alert(this.bindTo.tableName);
@@ -22,10 +26,9 @@ Vue.component("fd-control-bind-to", {
                 modal: true,
                 title: "选择绑定字段"
             });
-            //将当前对象附着到window上,提供给子窗体使用
-            window._SelectBindFieldObj = this;
         },
-        setSelectResultValue:function (result) {
+        setSelectFieldResultValue:function (result) {
+            this.bindTo.tableName=result;
             alert(result);
         }
     },
