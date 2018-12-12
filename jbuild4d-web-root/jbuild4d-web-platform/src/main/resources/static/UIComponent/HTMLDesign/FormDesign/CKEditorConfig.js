9ea686e9f4f0a196bab0d64a60968704040d0bb5
zhuibobo <zhuibobo@qq.com>
2018-12-12 16:03:31 +0800

    zhuangruibo

--- jbuild4d-web-root/jbuild4d-web-platform/src/main/resources/static/UIComponent/HTMLDesign/FormDesign/CKEditorConfig.js
+++ jbuild4d-web-root/jbuild4d-web-platform/src/main/resources/static/UIComponent/HTMLDesign/FormDesign/CKEditorConfig.js
@@ -42,7 +42,7 @@ CKEDITOR.editorConfig = function(config) {
     config.allowedContent = true;
     config.stylesSet = false;
     //config.autoParagraph = false;
-    //config.contentsCss = ['../../themes/cssloader/EditorViewDefaultStyle.resource','../../themes/cssloader/EditorViewDefaultStyle.resource','../../themes/cssloader/EditorDesignStyle.resource'];
+    config.contentsCss = ['../../../Themes/Default/FormDesign/FormDesignWysiwyg.css','../../../Themes/Default/FormDesign/FormResolve.css'];
     //,Source
     config.removeButtons = 'Maximize,Save,NewPage,Preview,Print,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Undo,Redo,' +
         'Replace,Find,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Bold,Italic,' +
