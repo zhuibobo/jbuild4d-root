42181d354ff6ace606419259cd8c27d4b6474981
zhuibobo <zhuibobo@qq.com>
2018-12-29 21:37:41 +0800

    zhuangrb

--- jbuild4d-web-root/jbuild4d-web-platform/src/main/resources/static/UIComponent/HTMLDesign/VueComponent/DialogPropComponent.js
+++ jbuild4d-web-root/jbuild4d-web-platform/src/main/resources/static/UIComponent/HTMLDesign/VueComponent/DialogPropComponent.js
@@ -913,17 +913,62 @@ Vue.component("db-table-relation-comp", {
 Vue.component("js-design-code-fragment", {
     data: function () {
         return {
+            jsEditorInstance:null
+        }
+    },
+    mounted:function(){
+    },
+    methods:{
+        setJSEditorInstance:function (obj) {
+            this.jsEditorInstance=obj;
+        },
+        getJsEditorInst:function(){
+            return this.jsEditorInstance;
+        },
+        insertJs:function(js){
+            var doc = this.getJsEditorInst().getDoc();
+            var cursor = doc.getCursor();
+            doc.replaceRange(js, cursor);
+        },
+        formatJS:function () {
+            CodeMirror.commands["selectAll"](this.getJsEditorInst());
+            var range = { from: this.getJsEditorInst().getCursor(true), to: this.getJsEditorInst().getCursor(false) };;
+            this.getJsEditorInst().autoFormatRange(range.from, range.to);
+        },
+        alertDesc:function () {
+
+        },
+        refScript:function () {
+            var js="<script type=\"text/javascript\" src=\"${contextPath}/UIComponent/TreeTable/Js/TreeTable.js\"></script>";
+            this.insertJs(js);
+        },
+        callServiceMethod:function () {
+
+        }
+    },
+    template: '<div class="js-code-fragment-wrap">\
+            <div class="js-code-fragment-item" @click="formatJS">格式化</div>\
+            <div class="js-code-fragment-item">说明</div>\
+            <div class="js-code-fragment-item" @click="refScript">引入脚本</div>\
+            <div class="js-code-fragment-item">获取URL参数</div>\
+            <div class="js-code-fragment-item">调用服务方法</div>\
+            <div class="js-code-fragment-item">加载数据字典</div>\
+        </div>'
+});
+
+/*html编辑器中的元素辅助列表*/
+Vue.component("design-html-elem-list", {
+    data: function () {
+        return {
         }
     },
     mounted:function(){
     },
     methods:{
     },
-    template: '<div class="js-code-fragment-wrap">\
-            <div class="js-code-fragment-item">格式化</div>\
-            <div class="js-code-fragment-item">说明</div>\
-            <div class="js-code-fragment-item">引入脚本</div>\
-            <div class="js-code-fragment-item">URL参数</div>\
-            <div class="js-code-fragment-item">调用服务方法</div>\
+    template: '<div class="design-html-elem-list-wrap">\
+            <div class="design-html-elem-list-item">格式化</div>\
+            <div class="design-html-elem-list-item">说明</div>\
         </div>'
 });
+
