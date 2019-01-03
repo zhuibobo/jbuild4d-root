ad441798c1c8388376b0fbd83b5ac4202620f99a
zhuibobo <zhuibobo@qq.com>
2019-01-03 17:25:52 +0800

    zhuangruibo

--- jbuild4d-front-end/build-jbuild4d-web-platform/buildtemplates.js
+++ jbuild4d-front-end/build-jbuild4d-web-platform/buildtemplates.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra')
 
-fs.copy('build-jbuild4d-web-platform/templates', '../jbuild4d-web-root/jbuild4d-web-platform/src/main/resources/templates1')
+fs.copy('build-jbuild4d-web-platform/templates', '../jbuild4d-web-root/jbuild4d-web-platform/src/main/resources/templates')
     .then(() => console.log('success!'))
     .catch(err => console.error(err));
 
