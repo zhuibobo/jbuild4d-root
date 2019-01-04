b51822097bc2cb838f1e2db1f6773c6bb57d868d
zhuibobo <zhuibobo@qq.com>
2019-01-04 22:01:23 +0800

    zhuangrb

--- jbuild4d-front-end/gulpfile.js
+++ jbuild4d-front-end/gulpfile.js
@@ -1,14 +1,22 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const gulpCopy = require('gulp-copy');
+
+const publicResourcePath="../jbuild4d-web-root/jbuild4d-web-platform/src/main/resources";
+const srcPlatformPath="build-jbuild4d-web-platform";
 
 gulp.task('default', done => {
     console.log('Start...................');
 
-    gulp.src('build-jbuild4d-web-platform/static/Js/JBuild4DBaseLib.js')
+    /*gulp.src('build-jbuild4d-web-platform/static/Js/JBuild4DBaseLib.js')
         .pipe(babel({
             presets: ['@babel/env']
         }))
-        .pipe(gulp.dest('build-jbuild4d-web-platform/dist'))
-    console.log('End...................');
+        .pipe(gulp.dest('build-jbuild4d-web-platform/dist'));*/
+
+    gulp.src(srcPlatformPath+"/templates/**/*", {base:"build-jbuild4d-web-platform/templates"})
+        .pipe(gulp.dest(publicResourcePath+"/templates"));
+
+    //console.log('End...................');
     done();
 });
