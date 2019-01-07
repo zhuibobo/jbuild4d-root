314f39ae96190a9ca890a1263c211ae958fff480
zhuibobo <zhuibobo@qq.com>
2019-01-07 18:04:55 +0800

    zhuangruibo

--- jbuild4d-front-end/gulpfile.js
+++ jbuild4d-front-end/gulpfile.js
@@ -34,19 +34,12 @@ gulp.task('default', done => {
 
     //gulp.src('build-jbuild4d-web-platform/dist/*.js', {base:"build-jbuild4d-web-platform/dist"}).pipe(gulp.dest(publicResourcePath+"/Js"));
 
-    /*拷贝HTML文件*/
-    gulp.src(srcPlatformStaticPath + "/Html/**/*", {base: srcPlatformStaticPath + "/Html"})
-        .pipe(replacecust(replaceBlockObj.replaceBlock('GeneralLib'), replaceBlockObj.replaceGeneralLib))
-        .pipe(replacecust(replaceBlockObj.replaceBlock('CodeMirrorLib'), replaceBlockObj.replaceCodeMirrorLib))
-        .pipe(replacecust(replaceBlockObj.replaceBlock('FormDesignLib'),replaceBlockObj.replaceFormDesignLib))
-        .pipe(replacecust(replaceBlockObj.replaceBlock('ZTreeExtendLib'), replaceBlockObj.replaceZTreeExtendLib))
-        .pipe(replacecust(replaceBlockObj.replaceBlock('ThemesLib'), replaceBlockObj.replaceThemesLib))
-        .pipe(gulp.dest(publicResourcePath + "/Html"));
+    copyAndResolveHtml(srcPlatformStaticPath + "/Html/**/*",srcPlatformStaticPath + "/Html",publicResourcePath + "/Html");
 
     //拷贝表单设计的相关的开发文件
     gulp.src([srcPlatformStaticPath + "/Js/HTMLDesign/FormDesign/**/*.js",srcPlatformStaticPath + "/Js/HTMLDesign/FormDesign/**/*.css",srcPlatformStaticPath + "/Js/HTMLDesign/FormDesign/**/*.png"], {base: "build-jbuild4d-web-platform/static/Js/HTMLDesign/FormDesign"}).
     pipe(gulp.dest(publicResourcePath + "/Js/HTMLDesign/FormDesign"));
-
+    copyAndResolveHtml(srcPlatformStaticPath + "/Js/HTMLDesign/FormDesign/**/*.html",srcPlatformStaticPath + "/Js/HTMLDesign/FormDesign",publicResourcePath + "/Js/HTMLDesign/FormDesign");
 
     //拷贝表单设计需要用到的Ckeditor-4.11.1
     //gulp.src(srcPlatformStaticPath+"/Js/HTMLDesign/Ckeditor-4.11.1/**/*", {base:"build-jbuild4d-web-platform/static/Js/HTMLDesign/Ckeditor-4.11.1"}).pipe(gulp.dest(publicResourcePath+"/Js/HTMLDesign/Ckeditor-4.11.1"));
@@ -55,8 +48,19 @@ gulp.task('default', done => {
     //gulp.src(srcPlatformStaticPath+"/Themes/**/*", {base:"build-jbuild4d-web-platform/static/Themes"}).pipe(gulp.dest(publicResourcePath+"/Themes"));
 
     /*拷贝第三方的JS库*/
-    gulp.src(srcPlatformStaticPath+"/Js/T3P/**/*", {base:"build-jbuild4d-web-platform/static/Js/T3P"}).pipe(gulp.dest(publicResourcePath+"/Js/T3P"));
+    //gulp.src(srcPlatformStaticPath+"/Js/T3P/**/*", {base:"build-jbuild4d-web-platform/static/Js/T3P"}).pipe(gulp.dest(publicResourcePath+"/Js/T3P"));
 
     //console.log('End...................');
     done();
 });
+
+function copyAndResolveHtml(sourcePath,base,toPath) {
+    /*拷贝HTML文件*/
+    gulp.src(sourcePath, {base: base})
+        .pipe(replacecust(replaceBlockObj.replaceBlock('GeneralLib'), replaceBlockObj.replaceGeneralLib))
+        .pipe(replacecust(replaceBlockObj.replaceBlock('CodeMirrorLib'), replaceBlockObj.replaceCodeMirrorLib))
+        .pipe(replacecust(replaceBlockObj.replaceBlock('FormDesignLib'), replaceBlockObj.replaceFormDesignLib))
+        .pipe(replacecust(replaceBlockObj.replaceBlock('ZTreeExtendLib'), replaceBlockObj.replaceZTreeExtendLib))
+        .pipe(replacecust(replaceBlockObj.replaceBlock('ThemesLib'), replaceBlockObj.replaceThemesLib))
+        .pipe(gulp.dest(toPath));
+}
