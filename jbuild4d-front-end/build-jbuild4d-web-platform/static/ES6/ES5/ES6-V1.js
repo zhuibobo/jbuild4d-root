f62a7a8c23629651039cd347328a63f9e702c9f6
zhuibobo <zhuibobo@qq.com>
2019-01-14 11:18:57 +0800

    zhuangrb

--- jbuild4d-front-end/build-jbuild4d-web-platform/static/ES6/ES5/ES6-V1.js
+++ jbuild4d-front-end/build-jbuild4d-web-platform/static/ES6/ES5/ES6-V1.js
@@ -2,5 +2,6 @@
 
 function a() {
   var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "2";
+  var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
   console.log(x);
 }
---
+++ jbuild4d-front-end/build-jbuild4d-web-platform/static/ES6/VariablesSet.js
@@ -0,0 +1,4 @@
+function example() {
+    return [1, 2, 3];
+}
+let [a, b, c] = example();
