887b43c88695b7459d68bfad5abac4e2cfcfda26
zhuibobo <zhuibobo@qq.com>
2019-02-19 15:59:18 +0800

    zhuangrb

--- jbuild4d-web-root/jbuild4d-web-platform/src/main/resources/static/Js/HTMLDesign/WebListDesign/Plugins/WLDCT_GroupBy/WLDCT_GroupByPlugin.js
+++ jbuild4d-web-root/jbuild4d-web-platform/src/main/resources/static/Js/HTMLDesign/WebListDesign/Plugins/WLDCT_GroupBy/WLDCT_GroupByPlugin.js
@@ -3,13 +3,7 @@
 
     CKEDITOR.plugins.add(CKEditorPluginUtility.Plugins[pluginName].Setting.SingleName, {
         init: function(editor) {
-            //点击确认时候指定的操作
-            /*function addToEditor(ckEditor, pluginSetting, props, contentWindow){
-                CKEditorPluginUtility.BuildGeneralElemToCKWysiwyg("<input type='text' />", pluginSetting, props, contentWindow);
-            }*/
-            //注册常规插件的操作//
-            //CKEditorPluginUtility.RegGeneralPluginToEditor(editor, this.path, CKEditorPluginUtility.Plugins[pluginName].Setting,addToEditor);
-            //alert("122221212");
+
             var pluginSetting=CKEditorPluginUtility.Plugins[pluginName].Setting;
             editor.ui.addButton(pluginSetting.SingleName, {
                 label: pluginSetting.ToolbarLabel,
