4ee9fb89d1523f6a8285481633fb61b8a1bdb660
zhuibobo <zhuibobo@qq.com>
2019-02-25 15:48:22 +0800

    zhuangrb

--- jbuild4d-platform-root/jbuild4d-platform-system/src/main/java/com/jbuild4d/platform/system/service/impl/HistoryDataServiceImpl.java
+++ jbuild4d-platform-root/jbuild4d-platform-system/src/main/java/com/jbuild4d/platform/system/service/impl/HistoryDataServiceImpl.java
@@ -1,16 +1,13 @@
 package com.jbuild4d.platform.system.service.impl;
-import java.util.Date;
-
 import com.jbuild4d.base.dbaccess.dao.systemsetting.HistoryDataMapper;
 import com.jbuild4d.base.dbaccess.dbentities.systemsetting.HistoryDataEntity;
-import com.jbuild4d.base.exception.JBuild4DGenerallyException;
-import com.jbuild4d.base.service.IAddBefore;
 import com.jbuild4d.base.service.ISQLBuilderService;
 import com.jbuild4d.base.service.general.JB4DSession;
-import com.jbuild4d.base.service.impl.BaseServiceImpl;
 import com.jbuild4d.base.tools.common.UUIDUtility;
 import com.jbuild4d.platform.system.service.IHistoryDataService;
 import org.mybatis.spring.SqlSessionTemplate;
+
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -22,14 +19,8 @@ public class HistoryDataServiceImpl implements IHistoryDataService
 {
     HistoryDataMapper historyDataMapper;
     public HistoryDataServiceImpl(HistoryDataMapper _defaultBaseMapper, SqlSessionTemplate _sqlSessionTemplate, ISQLBuilderService _sqlBuilderService){
-        //super(_defaultBaseMapper, _sqlSessionTemplate, _sqlBuilderService);
         historyDataMapper=_defaultBaseMapper;
     }
-
-    /*@Override
-    public int save(JB4DSession jb4DSession, String id, HistoryDataEntity record) throws JBuild4DGenerallyException {
-        throw
-    }*/
 
     @Override
     public void keepRecordData(JB4DSession jb4DSession, Object record){
