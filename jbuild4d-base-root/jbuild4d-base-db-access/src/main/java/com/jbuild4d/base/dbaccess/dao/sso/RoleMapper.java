f4c736a4459948fdf22d90676f47eeb6c2a14f43
zhuibobo <zhuibobo@qq.com>
2019-03-06 21:24:40 +0800

    zhuangrb

--- jbuild4d-base-root/jbuild4d-base-db-access/src/main/java/com/jbuild4d/base/dbaccess/dao/sso/RoleMapper.java
+++ jbuild4d-base-root/jbuild4d-base-db-access/src/main/java/com/jbuild4d/base/dbaccess/dao/sso/RoleMapper.java
@@ -2,9 +2,13 @@ package com.jbuild4d.base.dbaccess.dao.sso;
 
 import com.jbuild4d.base.dbaccess.dao.BaseMapper;
 import com.jbuild4d.base.dbaccess.dbentities.sso.RoleEntity;
+import org.apache.ibatis.annotations.Param;
 
 public interface RoleMapper extends BaseMapper<RoleEntity> {
 
     int countInRoleGroup(String groupId);
 
+    RoleEntity selectGreaterThanRecord(@Param("id") String id, @Param("groupId") String roleGroupId);
+
+    RoleEntity selectLessThanRecord(@Param("id") String id,@Param("groupId") String roleGroupId);
 }
--- jbuild4d-platform-root/jbuild4d-platform-sso/src/main/java/com/jbuild4d/platform/sso/service/impl/RoleServiceImpl.java
+++ jbuild4d-platform-root/jbuild4d-platform-sso/src/main/java/com/jbuild4d/platform/sso/service/impl/RoleServiceImpl.java
@@ -2,6 +2,8 @@ package com.jbuild4d.platform.sso.service.impl;
 import java.util.Date;
 
 import com.jbuild4d.base.dbaccess.dao.sso.RoleMapper;
+import com.jbuild4d.base.dbaccess.dbentities.devdemo.DevDemoGenListEntity;
+import com.jbuild4d.base.dbaccess.dbentities.devdemo.DevDemoTLTreeListEntity;
 import com.jbuild4d.base.dbaccess.dbentities.sso.RoleEntity;
 import com.jbuild4d.base.exception.JBuild4DGenerallyException;
 import com.jbuild4d.base.service.IAddBefore;
@@ -36,5 +38,29 @@ public class RoleServiceImpl extends BaseServiceImpl<RoleEntity> implements IRol
     @Override
     public int countInRoleGroup(String groupId) {
         return roleMapper.countInRoleGroup(groupId);
+    }
+
+    @Override
+    public void moveUp(JB4DSession jb4DSession, String id) throws JBuild4DGenerallyException {
+        RoleEntity selfEntity=roleMapper.selectByPrimaryKey(id);
+        RoleEntity ltEntity=roleMapper.selectGreaterThanRecord(id,selfEntity.getRoleGroupId());
+        switchOrder(ltEntity,selfEntity);
+    }
+
+    @Override
+    public void moveDown(JB4DSession jb4DSession, String id) throws JBuild4DGenerallyException {
+        RoleEntity selfEntity=roleMapper.selectByPrimaryKey(id);
+        RoleEntity ltEntity=roleMapper.selectLessThanRecord(id,selfEntity.getRoleGroupId());
+        switchOrder(ltEntity,selfEntity);
+    }
+
+    private void switchOrder(RoleEntity toEntity,RoleEntity selfEntity) {
+        if(toEntity !=null){
+            int newNum= toEntity.getRoleOrderNum();
+            toEntity.setRoleOrderNum(selfEntity.getRoleOrderNum());
+            selfEntity.setRoleOrderNum(newNum);
+            roleMapper.updateByPrimaryKeySelective(toEntity);
+            roleMapper.updateByPrimaryKeySelective(selfEntity);
+        }
     }
 }
