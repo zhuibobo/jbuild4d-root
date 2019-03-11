668d65735a7fc3ca1e967fd33ddcf8e694c6ea12
zhuibobo <zhuibobo@qq.com>
2019-03-11 19:34:38 +0800

    zhuangrb

--- jbuild4d-front-end/build-jbuild4d-web-platform/static/Js/VueComponent/sso-app-sub-system-list-comp.js
+++ jbuild4d-front-end/build-jbuild4d-web-platform/static/Js/VueComponent/sso-app-sub-system-list-comp.js
@@ -2,15 +2,63 @@
 Vue.component("sso-app-sub-system-list-comp", {
     data: function () {
         return {
+            items: [
+                { message: 'Foo' },
+                { message: 'Bar' },
+                { message: 'Foo' },
+                { message: 'Bar' },
+                { message: 'Foo' },
+                { message: 'Bar' }
+            ]
         }
     },
     mounted:function(){
 
     },
     methods:{
-        uploadSystemLogoImageSuccess:function (response, file, fileList) {
-
-        }
+        addIntegratedSystem:function() {
+            var elem=this.$refs.ssoAppInterfaceEditModelDialogWrap;
+            //debugger;
+            //this.getOrganDataInitTree();
+            DialogUtility.DialogElemObj(elem, {
+                modal: true,
+                width: 900,
+                height: 500,
+                title: "接口设置"
+            });
+        },
     },
-    template: ``
+    template: `<div>
+                    <div ref="ssoAppInterfaceEditModelDialogWrap" class="general-edit-page-wrap" style="display: none">
+                        <tabs>
+                            <tab-pane label="系统设置">
+                                <sso-app-detail-from-comp></sso-app-detail-from-comp>
+                            </tab-pane>
+                            <tab-pane label="接口设置">
+                                <sso-app-interface-list-comp></sso-app-interface-list-comp>
+                            </tab-pane>
+                        </tabs>
+                    </div>
+                    <div class="apps-manager-outer-wrap">
+                        <div class="apps-outer-wrap">
+                            <div v-for="item in items" class="app-outer-wrap">
+                                <div class="title">
+                                    <span>深圳市明天不知道干什么可能会下雨科技有限股份未明公司</span>
+                                </div>
+                                <div class="content">
+                                    <div class="mainImg">
+                                        <img src="../../../Themes/Default/Css/Images/DefaultSSOAppLogo.png" />
+                                    </div>
+                                    <div class="button">
+                                        系统设置
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="app-outer-wrap new-system-outer-wrap">
+                                <div class="add-system-button" @click="addIntegratedSystem()">添加集成系统</div>
+                                <div class="add-system-button" @click="addPostSystem()">添加模拟系统</div>
+                            </div>
+                        </div>
+                    </div>
+                 </div>`
 });
