1550102c97939dae3d28bb061afefc4ea43f675a
zhuibobo <zhuibobo@qq.com>
2019-03-13 21:57:18 +0800

    zhuangrb

---
+++ jbuild4d-web-root/jbuild4d-web-sso-client-demo1/src/main/java/com/jbuild4d/web/sso/demo1/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.jbuild4d.web.sso.demo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/Index")
+public class IndexController {
+
+    @RequestMapping(value = "/Welcome", method = RequestMethod.GET)
+    @ResponseBody
+    public String welcome() {
+       return "Index-Welcome3";
+    }
+}
