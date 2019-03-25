c8408dfc711f7ba2165013ade625a4375a5d7015
zhuibobo <zhuibobo@qq.com>
2019-03-25 17:35:48 +0800

    zhuangrb

--- jbuild4d-sso-root/jbuild4d-sso-java-web-client-demo-root/jbuild4d-sso-java-web-client-demo1/src/main/java/com/jbuild4d/sso/web/client/demo1/controller/IndexController.java
+++ jbuild4d-sso-root/jbuild4d-sso-java-web-client-demo-root/jbuild4d-sso-java-web-client-demo1/src/main/java/com/jbuild4d/sso/web/client/demo1/controller/IndexController.java
@@ -4,14 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/Index")
 public class IndexController {
 
     @RequestMapping(value = "/Welcome", method = RequestMethod.GET)
-    @ResponseBody
-    public String welcome() {
-        return "Index-Welcome";
+    public ModelAndView welcome() {
+        ModelAndView mv=new ModelAndView();
+        mv.setViewName("Welcome");
+        return mv;
+    }
+
+    @RequestMapping(value = "/Desktop", method = RequestMethod.GET)
+    public ModelAndView Desktop() {
+        ModelAndView mv=new ModelAndView();
+        mv.setViewName("Desktop");
+        return mv;
     }
 }
