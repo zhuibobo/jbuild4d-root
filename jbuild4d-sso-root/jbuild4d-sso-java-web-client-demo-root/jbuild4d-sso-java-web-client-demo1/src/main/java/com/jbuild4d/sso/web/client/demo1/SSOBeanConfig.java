f9dab4e9401ee46f5745acab86c51f32f03f642a
zhuibobo <zhuibobo@qq.com>
2019-03-25 17:56:42 +0800

    zhuangrb

--- jbuild4d-sso-root/jbuild4d-sso-java-web-client-demo-root/jbuild4d-sso-java-web-client-demo1/src/main/java/com/jbuild4d/sso/web/client/demo1/SSOBeanConfig.java
+++ jbuild4d-sso-root/jbuild4d-sso-java-web-client-demo-root/jbuild4d-sso-java-web-client-demo1/src/main/java/com/jbuild4d/sso/web/client/demo1/SSOBeanConfig.java
@@ -1,11 +1,17 @@
 package com.jbuild4d.sso.web.client.demo1;
 
+import com.jbuild4d.core.base.session.JB4DSession;
 import com.jbuild4d.sso.client.conf.Conf;
+import com.jbuild4d.sso.client.extend.ICheckSessionSuccess;
 import com.jbuild4d.sso.client.filter.SsoWebFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 
 @Configuration
 public class SSOBeanConfig {
@@ -33,7 +39,14 @@ public class SSOBeanConfig {
         registration.setName("XxlSsoWebFilter");
         registration.setOrder(1);
         registration.addUrlPatterns("/*");
-        registration.setFilter(new SsoWebFilter());
+        SsoWebFilter filter = new SsoWebFilter();
+        filter.setCheckSessionSuccess(new ICheckSessionSuccess() {
+            @Override
+            public void run(ServletRequest request, ServletResponse response, FilterChain chain, JB4DSession jb4DSession) {
+                System.out.println("重远程获取Session完成,用户:"+jb4DSession.getUserName());
+            }
+        });
+        registration.setFilter(filter);
         registration.addInitParameter(SsoWebFilter.KEY_SSO_SERVER, ssoServer);
         registration.addInitParameter(SsoWebFilter.KEY_SSO_LOGIN_PATH,ssoLoginPath);
         registration.addInitParameter(SsoWebFilter.KEY_SSO_LOGOUT_PATH, ssoLogoutPath);
