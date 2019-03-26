12213dfef611bb2238f20c0ce1eb01eaebf80c3d
zhuibobo <zhuibobo@qq.com>
2019-03-26 15:42:39 +0800

    zhuangrb

--- jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/datastorage/ITableRelationGroupService.java
+++ jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/datastorage/ITableRelationGroupService.java
@@ -2,5 +2,11 @@ package com.jbuild4d.platform.builder.datastorage;
 
 import com.jbuild4d.base.dbaccess.dbentities.builder.TableRelationGroupEntity;
 import com.jbuild4d.base.service.IBaseService;
+import com.jbuild4d.core.base.exception.JBuild4DGenerallyException;
+import com.jbuild4d.core.base.session.JB4DSession;
 
-public interface ITableRelationGroupService extends IBaseService<TableRelationGroupEntity> {}
+public interface ITableRelationGroupService extends IBaseService<TableRelationGroupEntity> {
+    String getRootId();
+
+    TableRelationGroupEntity createRootNode(JB4DSession jb4DSession) throws JBuild4DGenerallyException;
+}
--- jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/datastorage/impl/TableRelationGroupServiceImpl.java
+++ jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/datastorage/impl/TableRelationGroupServiceImpl.java
@@ -1,7 +1,9 @@
 package com.jbuild4d.platform.builder.datastorage.impl;
 
 import com.jbuild4d.base.dbaccess.dao.builder.TableRelationGroupMapper;
+import com.jbuild4d.base.dbaccess.dbentities.builder.TableGroupEntity;
 import com.jbuild4d.base.dbaccess.dbentities.builder.TableRelationGroupEntity;
+import com.jbuild4d.base.dbaccess.exenum.TrueFalseEnum;
 import com.jbuild4d.base.service.IAddBefore;
 import com.jbuild4d.base.service.ISQLBuilderService;
 import com.jbuild4d.base.service.impl.BaseServiceImpl;
@@ -12,10 +14,30 @@ import org.mybatis.spring.SqlSessionTemplate;
 
 public class TableRelationGroupServiceImpl extends BaseServiceImpl<TableRelationGroupEntity> implements ITableRelationGroupService
 {
+    @Override
+    public String getRootId() {
+        return rootId;
+    }
+
+    private String rootId="0";
+    private String rootParentId="-1";
+
     TableRelationGroupMapper tableRelationGroupMapper;
     public TableRelationGroupServiceImpl(TableRelationGroupMapper _defaultBaseMapper, SqlSessionTemplate _sqlSessionTemplate, ISQLBuilderService _sqlBuilderService){
         super(_defaultBaseMapper, _sqlSessionTemplate, _sqlBuilderService);
         tableRelationGroupMapper=_defaultBaseMapper;
+    }
+
+    @Override
+    public TableRelationGroupEntity createRootNode(JB4DSession jb4DSession) throws JBuild4DGenerallyException {
+        TableRelationGroupEntity treeTableEntity=new TableRelationGroupEntity();
+        treeTableEntity.setRelGroupId(rootId);
+        treeTableEntity.setRelGroupParentId(rootParentId);
+        treeTableEntity.setRelGroupIssystem(TrueFalseEnum.True.getDisplayName());
+        treeTableEntity.setRelGroupText("表分组");
+        treeTableEntity.setRelGroupValue("表分组");
+        this.saveSimple(jb4DSession,treeTableEntity.getRelGroupId(),treeTableEntity);
+        return treeTableEntity;
     }
 
     @Override
