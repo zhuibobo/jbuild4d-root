65b51994e5a701d7b22f40f46152b419d17f3a0e
zhuibobo <zhuibobo@qq.com>
2019-03-26 15:57:59 +0800

    zhuangrb

---
+++ jbuild4d-web-root/jbuild4d-web-platform/src/main/java/com/jbuild4d/web/platform/rest/builder/datastorage/tablerelation/TableRelationHistoryRestResource.java
@@ -0,0 +1,9 @@
+package com.jbuild4d.web.platform.rest.builder.datastorage.tablerelation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/PlatFormRest/Builder/DataStorage/TableRelation/TableRelationHistory")
+public class TableRelationHistoryRestResource {
+}
