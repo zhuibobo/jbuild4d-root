6c7d2535adaecbf9cbc7c184112389e7819fbd7a
zhuibobo <zhuibobo@qq.com>
2019-03-27 15:46:16 +0800

    zhuangrb

--- jbuild4d-front-end/build-jbuild4d-web-platform/static/Js/VueComponent/table-relation-content-comp.js
+++ jbuild4d-front-end/build-jbuild4d-web-platform/static/Js/VueComponent/table-relation-content-comp.js
@@ -3,6 +3,7 @@ Vue.component("table-relation-content-comp", {
     props:["relation"],
     data: function () {
         return {
+            tableRelationDiagram:null
         }
     },
     mounted:function(){
@@ -15,14 +16,15 @@ Vue.component("table-relation-content-comp", {
             if (window.goSamples) goSamples();  // init for these samples -- you don't need to call this
             var $ = go.GraphObject.make;  // for conciseness in defining templates
 
-            var myDiagram =
-                $(go.Diagram, "myDiagramDiv",  // must name or refer to the DIV HTML element
+            this.tableRelationDiagram =
+                $(go.Diagram, "tableRelationDiagramDiv",  // must name or refer to the DIV HTML element
                     {
-                        allowDelete: false,
+                        allowDelete: true,
                         allowCopy: false,
                         layout: $(go.ForceDirectedLayout),
                         "undoManager.isEnabled": true
                     });
+            var tableRelationDiagram=this.tableRelationDiagram;
 
             // define several shared Brushes
             var bluegrad = $(go.Brush, "Linear", { 0: "rgb(150, 150, 250)", 0.5: "rgb(86, 86, 186)", 1: "rgb(86, 86, 186)" });
@@ -47,7 +49,7 @@ Vue.component("table-relation-content-comp", {
                 );
 
             // define the Node template, representing an entity
-            myDiagram.nodeTemplate =
+            tableRelationDiagram.nodeTemplate =
                 $(go.Node, "Auto",  // the whole node panel
                     {
                         selectionAdorned: true,
@@ -58,7 +60,7 @@ Vue.component("table-relation-content-comp", {
                         isShadowed: true,
                         shadowColor: "#C5C1AA"
                     },
-                    new go.Binding("location", "location").makeTwoWay(),
+                    new go.Binding("location", "loc", go.Point.parse),
                     // whenever the PanelExpanderButton changes the visible property of the "LIST" panel,
                     // clear out any desiredSize set by the ResizingTool.
                     new go.Binding("desiredSize", "visible", function(v) { return new go.Size(NaN, NaN); }).ofObject("LIST"),
@@ -95,7 +97,7 @@ Vue.component("table-relation-content-comp", {
                 );  // end Node
 
             // define the Link template, representing a relationship
-            myDiagram.linkTemplate =
+            tableRelationDiagram.linkTemplate =
                 $(go.Link,  // the whole link panel
                     {
                         selectionAdorned: true,
@@ -129,6 +131,7 @@ Vue.component("table-relation-content-comp", {
                         new go.Binding("text", "toText"))
                 );
 
+
             // create the model for the E-R diagram
             var nodeDataArray = [
                 {
@@ -150,7 +153,8 @@ Vue.component("table-relation-content-comp", {
                     items: [{ name: "CategoryID", iskey: true, figure: "Decision", color: yellowgrad },
                         { name: "CategoryName", iskey: false, figure: "Cube1", color: bluegrad },
                         { name: "Description", iskey: false, figure: "Cube1", color: bluegrad },
-                        { name: "Picture", iskey: false, figure: "TriangleUp", color: redgrad }]
+                        { name: "Picture", iskey: false, figure: "TriangleUp", color: redgrad }],
+                    "loc": "-600.0094814408964 -104.49635001967278"
                 },
                 {
                     key: "Order Details",
@@ -162,17 +166,51 @@ Vue.component("table-relation-content-comp", {
                 },
             ];
             var linkDataArray = [
-                { from: "Products", to: "Suppliers", text: "0..N", toText: "1" },
-                { from: "Products", to: "Categories", text: "0..N", toText: "1" },
-                { from: "Order Details", to: "Products", text: "0..N", toText: "1" }
+                { from: "Products", to: "Suppliers", text: "[ProductID]0..N", toText: "[Discount]1",items:[{cdata:"1"}],cdata:"1" },
+                { from: "Products", to: "Categories", text: "[ProductID]0..N", toText: "[Discount]1",items:[{cdata:"1"}],cdata:"3" },
+                { from: "Order Details", to: "Products", text: "[ProductID]0..N", toText: "[Discount]1",items:[{cdata:"1"}],cdata:"2" }
             ];
-            myDiagram.model = $(go.GraphLinksModel,
+            tableRelationDiagram.model = $(go.GraphLinksModel,
                 {
                     copiesArrays: true,
                     copiesArrayObjects: true,
                     nodeDataArray: nodeDataArray,
                     linkDataArray: linkDataArray
                 });
+        },
+        deleteSelection:function () {
+            //debugger;
+            if (this.tableRelationDiagram.commandHandler.canDeleteSelection()) {
+                //alert("candel");
+                this.tableRelationDiagram.commandHandler.deleteSelection();
+                return;
+            }
+        },
+        connectSelectionNode:function () {
+            this.tableRelationDiagram.selection.each(function (part) {
+                if (part instanceof go.Node) {
+                    console.log(part.data);
+                }
+                else if (part instanceof go.Link) {
+                    console.log(part.data);
+                }
+            });
+        },
+        saveModel:function () {
+            alert("location -202 -170");
+            var node={
+                key: "Order Details11111",
+                loc:"-202 -170",
+                items: [{ name: "OrderID", iskey: true, figure: "Decision" },
+                    { name: "ProductID", iskey: true, figure: "Decision" },
+                    { name: "UnitPrice", iskey: false, figure: "MagneticData" },
+                    { name: "Quantity", iskey: false, figure: "MagneticData" },
+                    { name: "Discount", iskey: false, figure: "MagneticData" }]
+            };
+            this.tableRelationDiagram.model.addNodeData(node);
+
+            var json=this.tableRelationDiagram.model.toJson();
+            console.log(json);
         }
     },
     template: `<div ref="relationContentOuterWrap" class="table-relation-content-outer-wrap">
@@ -186,12 +224,14 @@ Vue.component("table-relation-content-comp", {
                             <div class="table-relation-op-buttons-inner-wrap">
                                 <button-group shape="circle">
                                     <i-button type="success" icon="md-add"></i-button>
+                                    <i-button @click="saveModel" type="primary" icon="logo-instagram"></i-button>
                                     <i-button type="primary" icon="md-create"></i-button>
-                                    <i-button type="primary" icon="md-close"></i-button>
+                                    <i-button @click="connectSelectionNode" type="success" icon="md-add">连接</i-button>
+                                    <i-button @click="deleteSelection" type="primary" icon="md-close"></i-button>
                                 </button-group>
                             </div>
                         </div>
                     </div>
-                    <div class="table-relation-content-wrap" id="myDiagramDiv"></div>
+                    <div class="table-relation-content-wrap" id="tableRelationDiagramDiv"></div>
                 </div>`
 });
