3307b3cba9a4828abdbd4cd3e0257fac9627544a
zhuibobo <zhuibobo@qq.com>
2019-04-09 10:53:38 +0800

    zhuangrb

--- jbuild4d-base-root/jbuild4d-base-db-access/src/main/resources/liquibase/创建数据库.sql
+++ jbuild4d-base-root/jbuild4d-base-db-access/src/main/resources/liquibase/创建数据库.sql
@@ -1,2 +1,2 @@
-drop database `jbuild4d_v07`;
-CREATE DATABASE  `jbuild4d_v07` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+drop database `jbuild4d_v08`;
+CREATE DATABASE  `jbuild4d_v08` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
