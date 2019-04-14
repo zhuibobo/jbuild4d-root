375a09f1f272d102775f182e2c61906e378904e0
zhuibobo <zhuibobo@qq.com>
2019-04-14 14:13:31 +0800

    zhuangrb

---
+++ jbuild4d-front-end-source/web-platform-admin/static/Js/HTMLDesign/WebListDesign/Plugins/WLDCT_ListSimpleSearchContainer/WLDCT_ListSimpleSearchContainerPlugin.js
@@ -0,0 +1,16 @@
+(function(pluginName){
+    CKEditorPluginUtility.Plugins[pluginName]=CKEditorPluginUtility.GetGeneralPluginInstance(pluginName,{});
+
+    CKEDITOR.plugins.add(CKEditorPluginUtility.Plugins[pluginName].Setting.SingleName, {
+        init: function(editor) {
+            //点击确认时候指定的操作
+            function addToEditor(ckEditor, pluginSetting, props, contentWindow){
+                //var controlDescText=CKEditorPluginUtility.GetControlDescText(pluginSetting,props);
+                var tip=CKEditorPluginUtility.GetAutoRemoveTipLabel("简单查询区域[双击编辑该部件],在下边div中编辑查询内容");
+                CKEditorPluginUtility.BuildGeneralElemToCKWysiwyg("<div class=\"wysiwyg-wldct-list-simple-search-outer-wrap\">"+tip+"<div class=\"wysiwyg-wldct-list-simple-search-inner-wrap\"></div></div>", pluginSetting, props, contentWindow);
+            }
+            //注册常规插件的操作
+            CKEditorPluginUtility.RegGeneralPluginToEditor(editor, this.path, CKEditorPluginUtility.Plugins[pluginName].Setting,addToEditor);
+        }
+    });
+})("WLDCT_ListSimpleSearchContainer");
