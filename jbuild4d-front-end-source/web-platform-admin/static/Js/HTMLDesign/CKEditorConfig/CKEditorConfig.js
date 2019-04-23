0a4d30f339a82010042fe81b95091d98f37b4c5a
zhuibobo <zhuibobo@qq.com>
2019-04-23 22:38:11 +0800

    zhuangrb

--- jbuild4d-front-end-source/web-platform-admin/static/Js/HTMLDesign/CKEditorConfig/CKEditorConfig.js
+++ jbuild4d-front-end-source/web-platform-admin/static/Js/HTMLDesign/CKEditorConfig/CKEditorConfig.js
@@ -37,7 +37,11 @@ CKEDITOR.editorConfig = function(config) {
         { name: 'LC_Template', groups: [] },
         { name: 'LC_Container', groups: [] },
         '/',
-        { name: 'LC_Simple', groups: [] }
+        { name: 'LC_Simple_G1', groups: [] },
+        '/',
+        { name: 'LC_Simple_G2', groups: [] },
+        '/',
+        { name: 'LC_Simple_G3', groups: [] }
     ];
     config.height=jQuery(".form-design-wraper").height()-112;
     config.fillEmptyBlocks = false;
---
+++ jbuild4d-web-root/jbuild4d-web-platform/src/main/resources/static/Js/HTMLDesign/WebListDesign/Plugins/WLDCT_FormButton/WLDCT_FormButtonPlugin.js
@@ -0,0 +1,15 @@
+(function(pluginName){
+    CKEditorPluginUtility.Plugins[pluginName]=CKEditorPluginUtility.GetGeneralPluginInstance(pluginName,{});
+
+    CKEDITOR.plugins.add(CKEditorPluginUtility.Plugins[pluginName].Setting.SingleName, {
+        init: function(editor) {
+            //点击确认时候指定的操作
+            function addToEditor(ckEditor, pluginSetting, props, contentWindow){
+                var controlDescText=props.normalProps.buttonCaption+"[窗体按钮]";
+                CKEditorPluginUtility.BuildGeneralElemToCKWysiwyg("<div class='wysiwyg-input-text'>"+controlDescText+"</div>", pluginSetting, props, contentWindow);
+            }
+            //注册常规插件的操作
+            CKEditorPluginUtility.RegGeneralPluginToEditor(editor, this.path, CKEditorPluginUtility.Plugins[pluginName].Setting,addToEditor);
+        }
+    });
+})("WLDCT_FormButton");
