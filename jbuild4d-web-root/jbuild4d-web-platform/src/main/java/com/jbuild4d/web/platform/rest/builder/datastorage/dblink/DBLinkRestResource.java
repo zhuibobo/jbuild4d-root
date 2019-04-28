b52f7bf1b386401ba67ca386b0a4f477b835adfb
zhuibobo <zhuibobo@qq.com>
2019-04-28 21:16:58 +0800

    zhuibobo

--- jbuild4d-web-root/jbuild4d-web-platform/src/main/java/com/jbuild4d/web/platform/rest/builder/datastorage/dblink/DBLinkRestResource.java
+++ jbuild4d-web-root/jbuild4d-web-platform/src/main/java/com/jbuild4d/web/platform/rest/builder/datastorage/dblink/DBLinkRestResource.java
@@ -1,12 +1,18 @@
 package com.jbuild4d.web.platform.rest.builder.datastorage.dblink;
 
 import com.jbuild4d.base.dbaccess.dbentities.builder.DbLinkEntity;
+import com.jbuild4d.base.dbaccess.dbentities.sso.OrganEntity;
 import com.jbuild4d.base.service.IBaseService;
+import com.jbuild4d.base.service.general.JB4DSessionUtility;
+import com.jbuild4d.core.base.vo.JBuild4DResponseVo;
 import com.jbuild4d.platform.builder.datastorage.IDbLinkService;
 import com.jbuild4d.web.platform.rest.base.GeneralRestResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/PlatFormRest/Builder/DataStorage/DBLink")
@@ -28,5 +34,11 @@ public class DBLinkRestResource extends GeneralRestResource<DbLinkEntity> {
     @Override
     protected IBaseService<DbLinkEntity> getBaseService() {
         return dbLinkService;
+    }
+
+    @RequestMapping(value = "/GetFullDBLink", method = RequestMethod.POST)
+    public JBuild4DResponseVo getFullOrgan() {
+        List<DbLinkEntity> dbLinkEntityList=dbLinkService.getALL(JB4DSessionUtility.getSession());
+        return JBuild4DResponseVo.success(JBuild4DResponseVo.GETDATASUCCESSMSG,dbLinkEntityList);
     }
 }
