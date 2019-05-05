fa45c81b3dcc2a953942ec6a337a8ac022a6e2a5
zhuibobo <zhuibobo@qq.com>
2019-05-05 22:33:07 +0800

    zhuibobo

--- jbuild4d-front-end-source/web-platform-admin/static/Js/HTMLDesign/WebListDesign/Plugins/WLDCT_ListSimpleSearchContainer/WLDCT_ListSimpleSearchContainerPlugin.js
+++ jbuild4d-front-end-source/web-platform-admin/static/Js/HTMLDesign/WebListDesign/Plugins/WLDCT_ListSimpleSearchContainer/WLDCT_ListSimpleSearchContainerPlugin.js
@@ -8,30 +8,30 @@
                 //var controlDescText=CKEditorPluginUtility.GetControlDescText(pluginSetting,props);
                 var tip = CKEditorPluginUtility.GetAutoRemoveTipLabel("简单查询区域[双击编辑该部件],在下边div中编辑查询内容");
                 CKEditorPluginUtility.BuildGeneralElemToCKWysiwyg("<div class=\"wysiwyg-wldct-list-simple-search-outer-wrap\">" + tip +
-                    "<div class=\"wysiwyg-wldct-list-simple-search-inner-wrap\">" +
-                    "<table>" +
-                    "    <colgroup>" +
-                    "        <col style=\"width: 8%\" />" +
-                    "        <col style=\"width: 17%\" />" +
-                    "        <col style=\"width: 8%\" />" +
-                    "        <col style=\"width: 17%\" />" +
-                    "        <col style=\"width: 8%\" />" +
-                    "        <col style=\"width: 17%\" />" +
-                    "        <col style=\"width: 8%\" />" +
-                    "        <col style=\"width: 17%\" />" +
-                    "    </colgroup>" +
-                    "    <tr>" +
-                    "        <td class=\"label\">名称:</td>" +
-                    "        <td></td>" +
-                    "        <td class=\"label\">标题:</td>" +
-                    "        <td></td>" +
-                    "        <td class=\"label\">时间(从):</td>" +
-                    "        <td></td>" +
-                    "        <td class=\"label\">(到):</td>" +
-                    "        <td></td>" +
-                    "    </tr>" +
-                    "</table>" +
-                    "</div>" +
+                    "   <div class=\"wysiwyg-wldct-list-simple-search-inner-wrap\">" +
+                    "       <table>" +
+                    "           <colgroup>" +
+                    "               <col style=\"width: 8%\" />" +
+                    "               <col style=\"width: 17%\" />" +
+                    "               <col style=\"width: 8%\" />" +
+                    "               <col style=\"width: 17%\" />" +
+                    "               <col style=\"width: 8%\" />" +
+                    "               <col style=\"width: 17%\" />" +
+                    "               <col style=\"width: 8%\" />" +
+                    "               <col style=\"width: 17%\" />" +
+                    "           </colgroup>" +
+                    "           <tr>" +
+                    "               <td class=\"label\">名称:</td>" +
+                    "               <td></td>" +
+                    "               <td class=\"label\">标题:</td>" +
+                    "               <td></td>" +
+                    "               <td class=\"label\">时间(从):</td>" +
+                    "               <td></td>" +
+                    "               <td class=\"label\">(到):</td>" +
+                    "               <td></td>" +
+                    "           </tr>" +
+                    "       </table>" +
+                    "   </div>" +
                     "</div>", pluginSetting, props, contentWindow);
             }
             //注册常规插件的操作
