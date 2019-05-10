fc9f979407a9a1b849da1aba0c0d789cc7d3e9da
zhuibobo <zhuibobo@qq.com>
2019-05-10 22:41:25 +0800

    zhuibobo

--- jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/htmldesign/control/weblist/WebListListButtonContainer.java
+++ jbuild4d-platform-root/jbuild4d-platform-builder/src/main/java/com/jbuild4d/platform/builder/htmldesign/control/weblist/WebListListButtonContainer.java
@@ -20,19 +20,35 @@ public class WebListListButtonContainer  extends HTMLControl implements IHTMLCon
     @Override
     public void rendererChain(JB4DSession jb4DSession, String sourceHTML, Document doc, Element singleControlElem, Element parentElem, Element lastParentJbuild4dCustomElem, ResolveHTMLControlContextVo resolveHTMLControlContextVo) throws JBuild4DGenerallyException {
 
+        StringBuilder buttonsJson=new StringBuilder();
+        buttonsJson.append("<script>");
         //获取其中的按钮,将html装换为json格式,提供给前端做二次解析.
         //获取所有的自定义控件
         Elements allElems=singleControlElem.getElementsByTag("div");
-        for (Element singleElem : allElems) {
-            if(singleElem.attr("jbuild4d_custom").equals("true")){
+        for (Element singleInnerElem : allElems) {
+            if(singleInnerElem.attr("jbuild4d_custom").equals("true")){
+                String singleName=singleInnerElem.attr(HTMLControlAttrs.SINGLENAME);
+                HtmlControlDefinitionVo htmlControlDefinitionVo=ckEditorPluginsService.getVo(singleName);
+                String serverResolveFullClassName = htmlControlDefinitionVo.getServerResolve();
+                if(serverResolveFullClassName!=null&&!serverResolveFullClassName.equals("")){
+                    try {
+                        IHTMLControl htmlControl = this.getHTMLControlInstance(serverResolveFullClassName);
 
+                        String buttonJson=htmlControl.parseToJson(jb4DSession, sourceHTML, doc, singleInnerElem, singleControlElem, lastParentJbuild4dCustomElem, resolveHTMLControlContextVo, htmlControlDefinitionVo);
+
+                        buttonsJson.append(buttonJson);
+                    }
+                    catch (Exception ex){
+                        singleInnerElem.html("控件解析出错！【"+ex.getMessage()+"】");
+                    }
+                }
             }
         }
-
+        buttonsJson.append("</script>");
         //查找其中的wldct-list-button-inner-wrap,将其内容清空
         Elements elements=singleControlElem.getElementsByClass("wldct-list-button-inner-wrap");
         if(elements!=null&&elements.size()>0){
-            elements.get(0).html("");
+            elements.get(0).html(buttonsJson.toString());
         }
 
         return;
